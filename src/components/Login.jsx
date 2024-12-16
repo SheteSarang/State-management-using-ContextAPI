00f6ccae55eacd9f6c6f8e39d0d2ff7223d14766
SheteSarang <sarang.shete@ncircletech.com>
2024-12-16 17:12:41 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,20 @@
 import React, { useState, useContext } from "react";
+
 import UserContext from "../context/UserContext"; // Import the context
+// This is the file which will send values to UserContextProvider,
+// Which will globally serve the values to different components.In this case-profile component.for this
+// useContext hook comes into picture
 
 function Login() {
     console.log("Login rendering...");
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
+    
+//Now here using the hook useContext. To save the data coming from UI in that UserContextProvider.jsx(i.e globally)
+// you are getting the access to {user} variable and its correponding state {setUser}.
+//Since you are updating the user, you will need its state. When u are fetching, only user variable will be sufficient
+//So if you want to update the state of variable, use- setUser
   const {setUser} = useContext(UserContext); // Access the setUser function from the context
 
   const handleSubmit = (e) => {
