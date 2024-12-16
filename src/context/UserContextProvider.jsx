78eed011c9c21e8c83f3e4fa2ca0c403b1645550
SheteSarang <sarang.shete@ncircletech.com>
2024-12-16 19:07:02 +0530

    comments added UserContextProvider.jsx

--- src/context/UserContextProvider.jsx
+++ src/context/UserContextProvider.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import UserContext from "./UserContext"; // Import the UserContext
 
-const UserContextProvider = ({ children }) => {
-  const [user, setUser] = useState(null); // State to hold the user data
+const UserContextProvider = ({ children }) => {                          //initializing the global thing with null values
+  const [user, setUser] = useState(null);  user is a variable         // So user is variable and state is a function. So you can pass both variable and function
   const [password, setPassword] = useState(null); 
   console.log("UserContextProvider rendering...");
 
   return (
-    <UserContext.Provider value={{ user, setUser, password, setPassword }}>
-      {children} {/* Pass children components */}
+    <UserContext.Provider value={{ user, setUser, password, setPassword }}>  {/*so UserContext will give you a major thing- a provider. A provider will be used as a sandwhich where inbetween you can put your components.*/}
+      {children} {/* here the components will come */}
     </UserContext.Provider>
   );
 };
